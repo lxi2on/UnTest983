5b59fb06ebee752e3458a132646ed7f83bd2166c
lxi2on <leonelparedestorres12@gmail.com>
2025-09-09 12:43:21 -0300

    Update DPMHVX2O93.lua

--- DPMHVX2O93.lua
+++ DPMHVX2O93.lua
@@ -63,7 +63,7 @@ local frame = Instance.new("Frame")
 
 frame.Size = UDim2.new(0, 265, 0, 340)
 
-frame.Position = UDim2.new(0, 550, 0, 150)
+frame.Position = UDim2.new(0, 500, 0, 150)
 
 frame.BackgroundTransparency = 0.5
 
